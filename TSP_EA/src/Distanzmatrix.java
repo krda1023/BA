cb1ea43c79b76a41b05441bbb09fad07cceae8b1
Daniel <Daniel@DESKTOP-JCI0GG8>
2018-04-06 17:51:58 +0200

    Commit

--- TSP_EA/src/Distanzmatrix.java
+++ TSP_EA/src/Distanzmatrix.java
@@ -1,8 +1,26 @@
+import java.util.Random;
 
-public class Distanzmatrix {
-  // hol dir ein Array oder Liste aus All Cities mit allen Städten
+// hol dir ein Array oder Liste aus All Cities mit allen Städten
 	// Erzeuge Send_Request und Übergebe alle Städte damit URL gebildet werden kann
 	//Klassenvariable Ergebnis[][] = request.callme()
 	// Methode die mich auf jeden Eintrag im Ergebnis genau zurück greifen lässt
 	//um später in getdistance() von Klasse Tour zugreifen zu können
+public class Distanzmatrix {
+	
+	public void erzeugeStadt ()
+	{	double max = 180.00;
+		double min = -180.00;
+		Random generator = new Random();
+		for (int i=0;i<50;i++)
+		{	  double x = min + (generator.nextDouble() * (max - min));
+			  double y = min + (generator.nextDouble() * (max - min));
+			  City neueStadt = new City(i,x,y);
+			  liste.addCity(neueStadt);
+		}
+	}
+	
+	
+	All_Cities liste = new All_Cities();
+	
+
 }
