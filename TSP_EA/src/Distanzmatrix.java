fb3295ce0a1b80c73a17d8ea8138f0e29d8aa6da
Daniel <Daniel@DESKTOP-JCI0GG8>
2018-04-06 18:12:56 +0200

    Stadt erzeugen, runden

--- TSP_EA/src/Distanzmatrix.java
+++ TSP_EA/src/Distanzmatrix.java
@@ -7,20 +7,31 @@ import java.util.Random;
 	//um später in getdistance() von Klasse Tour zugreifen zu können
 public class Distanzmatrix {
 	All_Cities liste = new All_Cities();
+	static double round(double wert)
+	{
+		double erg=Math.round(wert*Math.pow(10,5))/Math.pow(10, 5);;
+		return erg;
+	}
 	public void erzeugeStadt ()
 	{	double max = 180.00;
 		double min = -180.00;
 		Random generator = new Random();
 		for (int i=0;i<50;i++)
-		{	  double x = min + (generator.nextDouble() * (max - min));
-			  double y = min + (generator.nextDouble() * (max - min));
+		{	  double x_2 = min + (generator.nextDouble() * (max - min));
+			  double y_2 = min + (generator.nextDouble() * (max - min));
+			  double x= round(x_2);
+			  double y= round(y_2);
 			  City neueStadt = new City(i,x,y);
 			  liste.addCity(neueStadt);
+			  //System.out.println(x +" "+y);
 		}
 	}
 	
+	/*public static void main (String[]args)
+	{
 	
-	
-	
+	Distanzmatrix dis= new Distanzmatrix();
+	dis.erzeugeStadt();
 
+}*/
 }
